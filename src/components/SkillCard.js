1d7c6fe0f41e4879cf8b9a0a2becf25fa9119a7d
RohinBhat <bhatrohin003@gmail.com>
2021-09-21 12:59:44 +0530

    Minor fixes

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export const SkillCard = (props) => {
   const { name, url } = props.details;
   return (
-    <div className="w-28 bg-gray-600 py-4 rounded-md flex flex-col items-center">
+    <div className="w-28 my-3 mx-auto bg-gray-600 py-4 rounded-md flex flex-col items-center">
       <img className="w-12 mb-2" src={url} alt={name} />
       <p className="font-secondary text-white font-medium">{name}</p>
     </div>
