5c6fbb1586fccf8d54eae081b00ee21de7f3f459
RohinBhat <bhatrohin003@gmail.com>
2021-09-24 01:30:41 +0530

    Added Education and Experience

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,12 @@
+import Education from "../components/AboutMe/Education";
+import Experience from "../components/AboutMe/Experience";
 import React from "react";
 
 export default function AboutMe() {
-  return <div></div>;
+  return (
+    <div>
+      <Education />
+      <Experience />
+    </div>
+  );
 }
