1e1a6ff70d31c1dbd7ef448848bede4d41c574fd
RohinBhat <bhatrohin003@gmail.com>
2021-09-23 15:42:05 +0530

    Added attributions

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,17 +5,20 @@ import notfound from "../images/404.svg";
 export default function NotFound() {
   return (
     <div className="w-full mx-auto">
-      {/* <h1 className="text-center mt-2 font-primary font-black text-9xl capitalize text-gray-300 transition duration-500 ease-in-out transform hover:scale-105 hover:text-yellow-400">
-        404
-      </h1> */}
-      <div>
-        <img
-          className="mx-auto w-auto h-auto max-w-lg"
-          src={notfound}
-          alt="Not Found"
-        />
-      </div>
-      <h3 className="text-center font-primary font-black text-5xl text-white transition duration-500 ease-in-out transform hover:scale-105 hover:text-yellow-400">
+      <img
+        className="mx-auto w-auto h-auto max-w-lg"
+        src={notfound}
+        alt="Not Found"
+      />
+      <a
+        href="https://storyset.com/web"
+        target="_blank"
+        rel="noreferrer"
+        className="text-center font-normal font-secondary text-sm text-gray-400"
+      >
+        <p>Illustration by Storyset</p>
+      </a>
+      <h3 className="mt-4 text-center font-primary font-black text-5xl text-white transition duration-500 ease-in-out transform hover:scale-105 hover:text-yellow-400">
         Looks like you've reached a Dead End!
       </h3>
       <p className="text-center mt-2 font-secondary font-normal text-xl text-gray-400">
@@ -29,7 +32,7 @@ export default function NotFound() {
       </p>
       <div className="text-center">
         <Link to="/">
-          <button className="w-full mt-8 bg-transparent transition duration-500 ease-in-out hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-4 px-6 border border-blue-500 hover:border-transparent rounded lg:w-auto">
+          <button className="w-full mt-4 bg-transparent transition duration-500 ease-in-out hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-4 px-6 border border-blue-500 hover:border-transparent rounded lg:w-auto">
             Back to Home
           </button>
         </Link>
