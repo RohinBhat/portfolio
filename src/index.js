3d630102b5ed88fbc89775a4b46747e97eb23fb9
RohinBhat <bhatrohin003@gmail.com>
2022-02-24 19:55:50 +0530

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactGA from "react-ga";
-
-ReactGA.initialize("UA-221042328-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.Fragment>
